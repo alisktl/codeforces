962130c53dbb6421e64d3ccaf663d15d2846698d
alisktl <alisktl@gmail.com>
2021-03-20 06:35:34 +0600

    708.2 C1

---
+++ Codeforces Round #708 (Div. 2)/C1. k-LCM (easy version).cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    int N, K;
+    cin >> N >> K;
+
+    if(N == 3) {
+        cout << 1 << " " << 1 << " " << 1 << "\n";
+        return;
+    }
+
+    if(N & 1) {
+        cout << 1 << " " << (N/2) << " " << (N/2) << "\n";
+    } else if(N % 4 == 0) {
+        cout << (N/2) << " " << (N/4) << " " << (N/4) << "\n";
+    } else if(N % 2 == 0) {
+        cout << 2 << " " << ((N - 2)/2) << " " << ((N - 2)/2) << "\n";
+    }
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int T;
+	cin >> T;
+
+    for(int i = 0; i < T; i++) {
+        solve();
+    }
+
+	return 0;
+}
